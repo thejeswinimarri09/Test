879aebb34b7c805b907894943b74c91ca38fa37f
dev111 <dev111@gmail.com>
2026-03-28 11:16:46 -0700

    added f3.java in master

---
+++ f3.java
@@ -0,0 +1,14 @@
+class Mobile 
+{
+    String model;
+    Steing brand;
+    public static void main(String[] args)
+    {
+        Mobile m1 = new Moblie();
+        m1.model = "galaxy s24";
+        m1.brand = "samsung";
+
+        System.out.println("Mobile model : " + m1.model);
+        System.out.println("Mobile brand : " + m1.brand);
+    }
+}
