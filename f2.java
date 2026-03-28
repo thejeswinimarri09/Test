68743b96e982d04298fccc481b8be864ca444f15
dev111 <dev111@gmail.com>
2026-03-28 11:16:34 -0700

    added f2.java in master

---
+++ f2.java
@@ -0,0 +1,11 @@
+class Employee
+{
+    int age = 37;
+    int id = 1234;
+    public static void main(String[] args)
+    {
+        System.out.println("Employee age : " + age);
+        System.out.println("Employee id : " + id);
+
+    }
+}
